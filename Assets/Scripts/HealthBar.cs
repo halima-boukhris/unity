4400b16e661e9d9163701c9ef18b21a377a7447d
halima-boukhris <halima.boukhris@hotmail.com>
2026-03-11 11:01:00 +0100

    [UI] Création de la barre de vie dynamique colorée

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image fillImage;
+
+    [SerializeField]
+    private Gradient gradient;
+
+
+
+    public void SetHealth(float healthNormalized)
+    {
+        fillImage.fillAmount = healthNormalized; //permet de faire varier la taille de la barre de vie en fonction du pourcentage de vie restant
+        fillImage.color = gradient.Evaluate(healthNormalized); //permet de faire varier la couleur de la barre de vie en fonction du pourcentage de vie restant
+    }
+}
+ 
